8c6bc2c9882bf2b893188099ea237f665c4da13c
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 15:08:02 -0300

    Add Income class
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/irrf.py
+++ irrf/irrf.py
@@ -1,4 +1,9 @@
 
+class Income:
+    def __init__(self, value: int, description: str):
+        self.value = value
+        self.description = description
+
 class IRRF:
 
     def __init__(self) -> None:
