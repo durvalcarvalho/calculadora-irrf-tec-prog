adf0ffca1aa1cc8a5c4df3a58192d4c491f97f1d
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 16:04:03 -0300

    Modifies the IRRF class to save and return declared incomes
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/irrf.py
+++ irrf/irrf.py
@@ -1,8 +1,10 @@
 import numbers
 from typing import List
 from exceptions import InvalidIncomeValueError
+from functools import total_ordering
 
 
+@total_ordering
 class Income:
     def __init__(self, value: int, description: str):
         if not isinstance(value, numbers.Number) or value <= 0:
@@ -12,15 +14,31 @@ class Income:
         self.value = value
         self.description = description
 
+    def __eq__(self, other):
+        return self.value == other.value and self.description == other.description
+
+    def __lt__(self, other):
+        return self.value < other.value
+
 
 class IRRF:
 
     def __init__(self) -> None:
         self.total_income: float = 0
+        self._declared_incomes: List[Income] = []
 
     def register_income(self, value: float, description: str) -> None:
         self.total_income += value
 
+        self._declared_incomes.append(
+            Income(value=value, description=description),
+        )
+
+
     @property
     def declared_incomes(self) -> List[Income]:
-        return list()
+        return self._declared_incomes
+
+    @declared_incomes.setter
+    def declared_incomes(self, value: List[Income]) -> None:
+        raise RuntimeError("It is not allowed to change the list of declared income")
