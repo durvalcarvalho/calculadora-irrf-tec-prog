caa1d590b3667109c3e57cdd79969531243ba7c6
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 16:47:00 -0300

    Add parameterized test to calculate_tax method
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/test/test_irrf.py
+++ irrf/test/test_irrf.py
@@ -72,3 +72,71 @@ class IRRFTestCase(unittest.TestCase):
             list(self.irrf.declared_incomes),
             list(income_list),
         )
+
+    @parameterized.expand([
+        # test case 1
+        [ [ Income(value=1000.00, description='Weekly salary'), ], 0.0, ],
+
+        # test case 2
+        [ [ Income(value=1903.98, description='Weekly salary'), ], 0.0, ],
+
+        # test case 3
+        [ [ Income(value=1904.12, description='Weekly salary'), ], 0.1, ],
+
+        # test case 4
+        [ [ Income(value=2500.00, description='Weekly salary'), ], 44.70, ],
+
+        # test case 5
+        [ [ Income(value=2826.65, description='Weekly salary'), ], 69.20, ],
+
+        # test case 6
+        [ [ Income(value=3000.00, description='Weekly salary'), ], 95.20, ],
+
+        # test case 7
+        [ [ Income(value=3500.00, description='Weekly salary'), ], 170.20, ],
+
+        # test case 8
+        [ [ Income(value=3750.00, description='Weekly salary'), ], 207.70, ],
+
+        # test case 9
+        [ [ Income(value=3751.05, description='Weekly salary'), ], 207.86, ],
+
+        # test case 10
+        [ [ Income(value=3751.09, description='Weekly salary'), ], 207.86, ],
+
+        # test case 11
+        [ [ Income(value=4000.00, description='Weekly salary'), ], 263.87, ],
+
+        # test case 12
+        [ [ Income(value=4500.00, description='Weekly salary'), ], 376.37, ],
+
+        # test case 13
+        [ [ Income(value=4500.00, description='Weekly salary'), ], 376.37, ],
+
+        # test case 14
+        [ [ Income(value=4664.68, description='Weekly salary'), ], 413.42, ],
+
+        # test case 15
+        [ [ Income(value=4664.69, description='Weekly salary'), ], 413.42, ],
+
+        # test case 16
+        [ [ Income(value=5000.00, description='Weekly salary'), ], 505.64, ],
+
+        # test case 17
+        [ [ Income(value=6000.00, description='Weekly salary'), ], 780.64, ],
+
+        # test case 18
+        [ [ Income(value=7000.00, description='Weekly salary'), ], 1055.64, ],
+
+        # test case 19
+        [ [ Income(value=8000.00, description='Weekly salary'), ], 1330.64, ],
+
+        # test case 20
+        [ [ Income(value=9000.00, description='Weekly salary'), ], 1605.64, ],
+    ])
+    def test_tax_calculation(self, income_list: Tuple[Income], expected_tax: float):
+        income: Income
+        for income in income_list:
+            self.irrf.register_income(income.value, income.description)
+
+        self.assertEqual(self.irrf.calculate_tax(), expected_tax)
