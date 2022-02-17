766266c36cd6ffb74d46e21e024cf804dc6c7fc7
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 15:08:52 -0300

    Refactor `register_income_with_multiple_incomes` to be parameterized
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/test/test_irrf.py
+++ irrf/test/test_irrf.py
@@ -1,40 +1,51 @@
-from collections import namedtuple
+from typing import Tuple
 import unittest
 from parameterized import parameterized
 
-from irrf import IRRF
+from irrf import IRRF, Income
 
 
 class IRRFTestCase(unittest.TestCase):
-
-    Income = namedtuple('Income', ['value', 'description'])
 
     def setUp(self):
         self.irrf = IRRF()
 
     @parameterized.expand([
-        ( Income(value=1000, description='Weekly salary'), ),
-        ( Income(value=300, description='Interest on equity'), ),
-        ( Income(value=1500, description='Rent income'),  ),
+        [ Income(value=1000, description='Weekly salary'), ],
+        [ Income(value=300, description='Interest on equity'), ],
+        [ Income(value=1500, description='Rent income'),  ],
     ])
-    def test_register_income_1(self, income: Income, *args, **kwargs):
+    def test_register_income_1(self, income: Income):
         self.irrf.register_income(
             value=income.value,
             description=income.description,
         )
         self.assertEqual(self.irrf.total_income, income.value)
 
-    def test_register_income_with_multiple_incomes(self):
-        self.irrf.register_income(
-            value=1000,
-            description='Weekly salary',
-        )
-        self.irrf.register_income(
-            value=300,
-            description='Interest on equity',
-        )
-        self.irrf.register_income(
-            value=1500,
-            description='Rent income',
-        )
-        self.assertEqual(self.irrf.total_income, 2800)
+    @parameterized.expand([
+        [[
+            Income(value=1000, description='Weekly salary'),
+            Income(value=300, description='Interest on equity'),
+            Income(value=1500, description='Rent income'),
+        ]],
+
+        [[
+            Income(value=1750, description='Weekly salary'),
+            Income(value=13000, description='Car sale'),
+            Income(value=20000, description='Property sale'),
+        ]],
+    ])
+    def test_register_income_with_multiple_incomes(self, income_list: Tuple[Income], *args, **kwargs):
+
+        total_income = 0
+
+        income: Income
+        for income in income_list:
+            total_income += income.value
+
+            self.irrf.register_income(
+                value=income.value,
+                description=income.description,
+            )
+
+        self.assertEqual(self.irrf.total_income, total_income)
