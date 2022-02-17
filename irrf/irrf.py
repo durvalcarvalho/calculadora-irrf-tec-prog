72cfa1dbf30e3a0a6cbf866f070399bae9441c84
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 14:40:44 -0300

    Implements the register_income method using the `falsification` technique
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/irrf.py
+++ irrf/irrf.py
@@ -2,4 +2,7 @@
 class IRRF:
 
     def __init__(self) -> None:
-        pass
+        self.total_income: float = 0
+
+    def register_income(self, value: float, description: str) -> None:
+        self.total_income = value
