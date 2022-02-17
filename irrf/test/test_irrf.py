674998c2d711891640d263aa13cd05f3ff00415c
Durval Carvalho <durvalcsouza@outlook.com>
2022-02-17 14:41:11 -0300

    Add more tests using the `triangulation` technique
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Hugo Sobral <hugosobral10@gmail.com>

--- irrf/test/test_irrf.py
+++ irrf/test/test_irrf.py
@@ -9,9 +9,23 @@ class IRRFTestCase(unittest.TestCase):
     def setUp(self):
         self.irrf = IRRF()
 
-    def test_register_income(self):
+    def test_register_income_1(self):
         self.irrf.register_income(
             value=1000,
             description='Weekly salary',
         )
         self.assertEqual(self.irrf.total_income, 1000)
+
+    def test_register_income_2(self):
+        self.irrf.register_income(
+            value=300,
+            description='Interest on equity',
+        )
+        self.assertEqual(self.irrf.total_income, 300)
+
+    def test_register_income_3(self):
+        self.irrf.register_income(
+            value=1500,
+            description='Rent income',
+        )
+        self.assertEqual(self.irrf.total_income, 1500)
