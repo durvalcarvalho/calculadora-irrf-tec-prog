adc8af1ca92ef9eb56e04d54a1d4dc5782e110db
Hugo Sobral <hugosobral10@gmail.com>
2022-02-25 10:58:05 -0300

    Implement official pension test using DUPLICATION technique
    Co-authored-by: Hérick Portugues <herick.portugues@gmail.com>
    Co-authored-by: Lucas Ursulino Boaventura <lucasxboaventura@hotmail.com>
    Co-authored-by: Leonardo Gomes <leonardodasigomes@gmail.com>
    Co-authored-by: Durval Carvalho <durvalcsouza@outlook.com>

--- irrf/test/test_deduction.py
+++ irrf/test/test_deduction.py
@@ -10,3 +10,7 @@ class TestDeduction(unittest.TestCase):
     def test_register_official_pension(self):
         self.irrf.register_official_pension("Contribuicao compulsoria", 1000.0)
         self.assertEqual(self.irrf.get_total_official_pension(), 1000.0)
+
+    def test_another_register_official_pension(self):
+        self.irrf.register_official_pension("Contribuicao compulsoria", 800.0)
+        self.assertEqual(self.irrf.get_total_official_pension(), 800.0)
